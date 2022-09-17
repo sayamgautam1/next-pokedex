e15d70a1ef3f086ebce2f2b49b21d9f63c2665b2
Sayam Gautam <sayamgautam1@gmail.com>
2022-09-17 17:43:04 +0930

    get kanto 151 pokemon data

--- pages/compnents/Header.js
+++ pages/compnents/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Header = () => {};
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-5">
+      <img></img>
+      <h1 className="text-3xl">POKEDEX</h1>
+    </div>
+  );
+};
 
 export default Header;
