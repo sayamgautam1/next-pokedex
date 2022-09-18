26043871ef8d7e44a7e0049a087d9147c32818b4
Sayam Gautam <sayamgautam1@gmail.com>
2022-09-18 18:58:23 +0930

    add kanto and jhoto pokemons, navigate through home page

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,38 @@
 import Header from "./compnents/Header";
 import Link from "next/Link";
 
-export default function Home({ pokemon }) {
+export default function Home() {
   return (
     <>
       <Header title={"POKEDEX"} />
-      <main className="px-4 py-12">
-        <h1 className="text-5xl text-green-500 text-center">
-          Pokedex - Gotta Catch em all!
-        </h1>
-        <div className="flex flex-wrap w-full justify-center mt-8">
-          {pokemon.map((pokeman, index) => (
-            <Link href={`/pokemon?id=${index + 1}`}>
-              <div className="mr-3 mb-4 bg-gray-200 p-3 rounded text-center cursor-pointer">
-                <div className="w-32">
-                  <img src={pokeman.image} alt={pokeman.name} />
-                </div>
-                <div className="mr-2 font-bold capitalize">{pokeman.name}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
+      <main className="px-4 py-12 flex flex-wrap w-full justify-center mt-8">
+        <Link href="./kanto">
+          <div class="max-w-lg bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  w-screen m-5 cursor-pointer">
+            <img
+              class="rounded-full -mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 md:h-32 md:w-32 border-[6px] border-white bg-white "
+              src="https://archives.bulbagarden.net/media/upload/2/25/LGPE_Kanto_Map.png"
+              alt=""
+            />
+            <h5 class="m-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+              KANTO
+            </h5>
+          </div>
+        </Link>
+        <Link href="./jhoto">
+          <div class="max-w-lg bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  w-screen m-5 cursor-pointer">
+            <img
+              class="rounded-full -mt-10 md:-mt-16 ml-3 md:ml-0 h-20 w-20 md:h-32 md:w-32 border-[6px] border-white bg-white"
+              src="https://archives.bulbagarden.net/media/upload/6/64/JohtoMap.png"
+              alt=""
+            />
+
+            <h5 class="m-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+              JHOTO
+            </h5>
+          </div>
+        </Link>
       </main>
       ;
     </>
   );
-}
-
-export async function getStaticProps(context) {
-  try {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
-    const { results } = await res.json();
-    const pokemon = results.map((pokeman, index) => {
-      const paddedId = ("00" + (index + 1)).slice(-3);
-
-      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
-      return { ...pokeman, image };
-    });
-    return {
-      props: { pokemon },
-    };
-  } catch (err) {
-    console.error(err);
-  }
 }
